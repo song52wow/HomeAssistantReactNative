b30a1f6d23e628fb7162bac7aa68d35357c563c3
Hari <619202904@qq.com>
2021-04-29 18:59:16 +0800

    侧边栏导航

--- App.tsx
+++ App.tsx
@@ -8,38 +8,52 @@
  * @format
  */
 
-import 'react-native-gesture-handler'
+import "react-native-gesture-handler";
 import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack';
-
+import { createStackNavigator, StackNavigationOptions } from "@react-navigation/stack";
 
 import Welcome from "./src/views/Welcome";
-import ScanNetwork from './src/views/ScanNetwork';
-import SetNetwork from './src/views/SetNetwork';
-import Authen from './src/views/Authen';
+import ScanNetwork from "./src/views/ScanNetwork";
+import SetNetwork from "./src/views/SetNetwork";
+import Authen from "./src/views/Authen";
+import { Alert, Text, View } from "react-native";
+import { createDrawerNavigator, DrawerNavigationOptions } from "@react-navigation/drawer";
 
-const Stack = createStackNavigator()
+interface INavList {
+  [key: string]: {
+    component: React.ComponentType<any>;
+    options?: DrawerNavigationOptions;
+  };
+}
+
+const navList: INavList = {
+  Welcome: {
+    component: Welcome,
+  },
+  ScanNetwork: {
+    component: ScanNetwork
+  },
+  SetNetwork: {
+    component: SetNetwork
+  },
+  Authen: {
+    component: Authen
+  }
+};
 
 const App = () => {
-  const navList = {
-    Welcome,
-    ScanNetwork,
-    SetNetwork,
-    Authen
-  }
-
+  const Drawer = createDrawerNavigator();
+  // const Stake = createStackNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {
-          Object.entries(navList).map(([key, value]) => (
-            <Stack.Screen key={key} name={key} component={value} />
-          ))
-        }
-      </Stack.Navigator>
+      <Drawer.Navigator>
+        {Object.entries(navList).map(([key, value]) => (
+          <Drawer.Screen key={key} name={key} component={value.component} />
+        ))}
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
